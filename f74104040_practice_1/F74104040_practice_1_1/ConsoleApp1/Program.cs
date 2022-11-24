aa8c0d207e04856b006d6afae1e4da74a823f1f0
mambahuang <angela98016@gmail.com>
2022-11-25 07:51:16 +0800

    first console commit

---
+++ f74104040_practice_1/F74104040_practice_1_1/ConsoleApp1/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int func;
+            int currentmoney = 0;
+            int money;
+            int type;
+            double allex = 0;
+            //five types of money
+            double alleat = 0;
+            double allcloth = 0;
+            double alllive = 0;
+            double allwalk = 0;
+            double allplay = 0;
+
+            do
+            {
+                Console.WriteLine("(1)輸入收入 (2)輸入支出 (3)計算比例 (4)剩餘金額 (5)退出程式");
+                Console.Write("輸入數字選擇功能: ");
+                func = Convert.ToInt32(Console.ReadLine());
+                if (func == 1)
+                {
+                    currentmoney = Income(currentmoney);
+                }
+                else if (func == 2)
+                {
+                    Console.WriteLine("(1)食 (2)衣 (3)住 (4)行 (5)育樂");
+                    Console.Write("輸入數字選擇支出項目: ");
+                    type = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("輸入支出金額: ");
+                    money = Convert.ToInt32(Console.ReadLine());
+                    switch (type)
+                    {
+                        case 1:
+                            currentmoney -= money;
+                            alleat += money;
+                            break;
+                        case 2:
+                            currentmoney -= money;
+                            allcloth += money;
+                            break;
+                        case 3:
+                            currentmoney -= money;
+                            alllive += money;
+                            break;
+                        case 4:
+                            currentmoney -= money;
+                            allwalk += money;
+                            break;
+                        case 5:
+                            currentmoney -= money;
+                            allplay += money;
+                            break;
+                    }
+                }
+                else if (func == 3)
+                {
+                    if(alleat != 0 || allcloth != 0 || alllive !=0 || allwalk != 0 || allplay != 0)
+                    {
+                        allex = alleat + allcloth + alllive + allwalk + allplay;
+                        Console.WriteLine(allex);
+                        Console.WriteLine(alleat);
+                        Console.WriteLine("食: " + (alleat / allex) * 100 + "%");
+                        Console.WriteLine("衣: " + (allcloth / allex) * 100 + "%");
+                        Console.WriteLine("住: " + (alllive / allex) * 100 + "%");
+                        Console.WriteLine("行: " + (allwalk / allex) * 100 + "%");
+                        Console.WriteLine("育樂: " + (allplay / allex) * 100 + "%");
+                    }
+                    else
+                    {
+                        Console.WriteLine("食: 0%");
+                        Console.WriteLine("衣: 0%");
+                        Console.WriteLine("住: 0%");
+                        Console.WriteLine("行: 0%");
+                        Console.WriteLine("育樂: 0%");
+                    }
+                    
+                }
+                else if (func == 4)
+                {
+                    Console.WriteLine("剩餘金額為: " + currentmoney);
+                }
+                else if (func == 5)
+                {
+                    ;
+                }
+                Console.WriteLine();
+            } while (func != 5);
+            
+        }
+        static int Income(int currentmoney)
+        {
+            int money;
+            Console.Write("輸入金額: ");
+            money = Convert.ToInt32(Console.ReadLine());
+            currentmoney += money;
+            return currentmoney;
+        }
+
+    }
+}
---
+++ f74104040_practice_1/F74104040_practice_1_2/ConsoleApp2/Program.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int func;
+            int currentmoney = 0;
+            string money;
+            int num_money;
+            int type;
+            string[] item = new string[5];
+            int[] flag = { 0, 0, 0, 0, 0 };
+            int[] item_money = { 0, 0, 0, 0, 0 };
+            int[] item_total = { 0, 0, 0, 0, 0 };
+            double total = 0;
+
+            //check  if the value is valid
+            bool valid;
+
+            string tmp1;
+            string tmp2;
+
+            int tmp3;
+            int tmp4;
+            int tmp5;
+
+            for (int i=0; i<5; i++)
+            {
+                item[i] = "";
+            }
+
+            do
+            {
+                Console.WriteLine("(1)輸入收入 (2)輸入支出 (3)新增項目 (4)刪除項目 (5)計算比例 (6) 查詢支出 (7)剩餘金額 (8)退出程式");
+                Console.Write("輸入數字選擇功能: ");
+                func = Convert.ToInt32(Console.ReadLine());
+                if (func == 1)
+                {
+                    Console.Write("輸入金額: ");
+                    money = Console.ReadLine();
+                    valid = int.TryParse(money, out int value);
+                    if (valid)
+                    {
+                        num_money = Convert.ToInt32(money);
+                        if (num_money >= 0)
+                        {
+                            currentmoney += num_money;
+                        }
+                        else
+                        {
+                            Console.WriteLine("收入不可為負數");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("請輸入數字");
+                    }
+                    
+                }
+                else if (func == 2)
+                {
+                    if (string.IsNullOrEmpty(item[0]))
+                    {
+                        Console.WriteLine("請新增支出項目");
+                    }
+                    else
+                    {
+                        for(int i=0; i<item.Length; i++)
+                        {
+                            if(!item[i].Equals(""))
+                                Console.WriteLine("(" + (i + 1) + ")" + item[i]);
+                        }
+                        Console.Write("輸入數字選擇支出項目: ");
+                        type = Convert.ToInt32(Console.ReadLine());
+                        if(type <= 0 || type > item.Length)
+                        {
+                            Console.WriteLine("此數字不再範圍中");
+                        }
+                        else
+                        {
+                            if (item[type-1] == "")
+                            {
+                                Console.WriteLine("此數字不再範圍中");
+                            }
+                            else
+                            {
+                                Console.Write("輸入支出金額: ");
+                                item_money[type-1] = Convert.ToInt32(Console.ReadLine());
+                                if (currentmoney - item_money[type-1] < 0)
+                                {
+                                    Console.WriteLine("存款不足");
+                                }
+                                else
+                                {
+                                    currentmoney -= item_money[type-1];
+                                    item_total[type-1] += item_money[type-1];
+                                }
+                            }
+                        }
+                    }
+                }else if(func == 3)
+                {
+                    if (!string.IsNullOrEmpty(item[item.Length - 1]))
+                    {
+                        Console.WriteLine("已無法再新增支出項目");
+                    }
+                    else
+                    {
+                        Console.Write("輸入項目名稱: ");
+                        tmp1 = Console.ReadLine();
+                        for (int i = 0; i < flag.Length; i++)
+                        {
+                            if (flag[i] == 1)
+                            {
+                                if (tmp1.Equals(item[i]))
+                                {
+                                    Console.WriteLine("支出項目已存在");
+                                    break;
+                                }
+                                else
+                                {
+                                    continue;
+                                }
+                                
+                            }
+                            else
+                            {
+                                item[i] = tmp1;
+                                flag[i] = 1;
+                                break;
+                            }
+                        }
+                    }
+
+                }else if(func == 4)
+                {
+                    string del;
+                    if (string.IsNullOrEmpty(item[0]))
+                    {
+                        Console.WriteLine("已無法再刪除支出項目");
+                    }
+                    else
+                    {
+                        Console.Write("輸入項目名稱: ");
+                        del = Console.ReadLine();
+                        int i;
+                        for (i = 0; i < item.Length; i++)
+                        {
+                            if (item[i] == del)
+                            {
+                                item[i] = "";
+                                item_money[i] = 0;
+                                item_total[i] = 0;
+                                flag[i] = 0;
+
+                                if (i != item.Length - 1)
+                                {
+                                    // 1 space forward
+                                    for (int j = i + 1; j < item.Length; j++)
+                                    {
+                                        tmp2 = item[j];
+                                        item[j] = "";
+                                        item[j - 1] = tmp2;
+
+                                        tmp3 = item_money[j];
+                                        item_money[j] = 0;
+                                        item_money[j - 1] = tmp3;
+
+                                        tmp4 = item_total[j];
+                                        item_total[j] = 0;
+                                        item_total[j - 1] = tmp4;
+
+                                        tmp5 = flag[j];
+                                        flag[j] = 0;
+                                        flag[j - 1] = tmp5;
+                                    }
+                                    break;
+                                }
+                                else
+                                {
+                                    //last item
+                                    item[i] = "";
+                                    item_money[i] = 0;
+                                    item_total[i] = 0;
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                        if(i == item.Length)
+                            Console.WriteLine("此項目不存在");
+                    }
+                    
+                }
+                else if (func == 5)
+                {
+                    //total expenditure
+                    total = 0;
+                    for (int i = 0; i<item_total.Length; i++)
+                    {
+                        total += item_total[i];
+                    }
+                    for (int i = 0; i < item.Length; i++)
+                    {
+                        if(item[i] != "")
+                            Console.WriteLine("(" + (i + 1) + ")" + item[i] + ": " + (item_total[i] / total)*100 + "%");
+                    }
+                }
+                else if(func == 6)
+                {
+                    string search;
+                    Console.WriteLine("目前總支出: " + total);
+                    Console.Write("輸入要查詢的項目: ");
+                    search = Console.ReadLine();
+                    int i;
+                    for(i=0; i<item.Length; i++)
+                    {
+                        if (search.Equals(item[i]))
+                        {
+                            Console.WriteLine(item[i] + ": " + item_total[i]);
+                            break;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                    if(i == item.Length)
+                        Console.WriteLine("此項目不存在");
+                }
+                else if (func == 7)
+                {
+                    Console.WriteLine("剩餘金額為: " + currentmoney);
+                }
+                else if (func == 8)
+                {
+                    ;
+                }
+                Console.WriteLine();
+            } while (func != 8);
+        }
+    }
+}
