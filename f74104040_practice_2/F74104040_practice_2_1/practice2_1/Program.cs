770b957204fa4120ecd009f68bb050647abcb8c4
mambahuang <angela98016@gmail.com>
2022-11-25 08:33:12 +0800

    second upload

---
+++ f74104040_practice_2/F74104040_practice_2_1/practice2_1/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int func;
+            int credits = 0;
+            bool flag2 = true;
+            string info;
+            string delete;
+            string[] subs = new string[4];
+            int checkpos = 0;
+
+            //initialize
+            string[] check = new string[72];
+            for (int i = 0; i < check.Length; i++)
+                check[i] = "";
+
+            //initialize
+            string[,] curri = new string[9,8];
+            for(int i = 0; i<9; i++)
+            {
+                for(int j=0; j<8; j++)
+                {
+                    curri[i, j] = "";
+                }
+            }
+
+            curri[0, 0] = "";
+            curri[1, 0] = "1";
+            curri[2, 0] = "2";
+            curri[3, 0] = "3";
+            curri[4, 0] = "4";
+            curri[5, 0] = "5";
+            curri[6, 0] = "6";
+            curri[7, 0] = "7";
+            curri[8, 0] = "8";
+
+            //Everyday
+            curri[0, 0] = "";
+            curri[0, 1] = "Sun";
+            curri[0, 2] = "Mon";
+            curri[0, 3] = "Tue";
+            curri[0, 4] = "Wed";
+            curri[0, 5] = "Thu";
+            curri[0, 6] = "Fri";
+            curri[0, 7] = "Sat";
+
+            do
+            {
+                Console.WriteLine("(1)Add a course (2)Delete a course (3)Print timetable (4)Computing credits  (5)Exit");
+                Console.Write("Input number selection function: ");
+                func = Convert.ToInt32(Console.ReadLine());
+                if(func == 1)
+                {
+                    Console.WriteLine("Please Input the course you want to add, format is <Course code, Mon~Sun, Start, End>");
+                    info = Console.ReadLine();
+                    subs = info.Split(' ');
+
+                    for(int j=0; j<check.Length; j++){
+                        if (subs[0] == check[j])
+                        {
+                            flag2 = true;
+                            Console.WriteLine("Course repetition");
+                            break;
+                        }
+                        if(j == check.Length - 1)
+                        {
+                            flag2 = false;
+                        }
+                    }
+
+                    //Console.WriteLine(flag2);
+                    if (!flag2)
+                    {
+                        for (int i = Convert.ToInt32(subs[2]); i <= Convert.ToInt32(subs[3]); i++)
+                        {
+                            if (curri[i, Convert.ToInt32(subs[1]) + 1] != "")
+                            {
+                                Console.WriteLine("Course redemption");
+                                break;
+                            }
+                            else if (i == Convert.ToInt32(subs[3]))
+                            {
+                                check[checkpos] = subs[0];
+                                checkpos++;
+                                for (int j = Convert.ToInt32(subs[2]); j <= Convert.ToInt32(subs[3]); j++)
+                                {
+                                    curri[j, Convert.ToInt32(subs[1]) + 1] = subs[0];
+                                }
+                                Console.WriteLine("Success to add the course");
+                                credits += Convert.ToInt32(subs[3]) - Convert.ToInt32(subs[2]) + 1;
+                            }
+                        }
+                    }
+
+                }
+                else if(func == 2)
+                {
+                    Console.Write("Please enter the course code you want to delete: ");
+                    delete = Console.ReadLine();
+                    bool flag = false;
+                    for(int k= 0; k<check.Length; k++)
+                    {
+                        //remove the record
+                        if (check[k] == delete)
+                        {
+                            flag = true;
+                            for(int i=0; i<check.Length; i++)
+                            {
+                                string tmp = "";
+                                if(check[i] == delete)
+                                {
+                                    check[i] = "";
+                                    for(int j=i+1; j<check.Length; j++)
+                                    {
+                                        tmp = check[j];
+                                        check[j] = check[j - 1];
+                                        check[j - 1] = tmp;
+                                    }
+                                }
+                                
+                            }
+                            //delete curriculum
+                            for(int i=1; i<9; i++)
+                            {
+                                for(int j=1; j<8; j++)
+                                {
+                                    if(curri[i,j] == delete)
+                                    {
+                                        curri[i,j] = "";
+                                        credits--;
+                                    }
+                                }
+                            }
+                            Console.WriteLine("Successfully delete the course: " + delete);
+                            break;
+                        }
+                        
+                    }
+                    if(!flag)
+                        Console.WriteLine("Course " + delete + " is not in the curriculum");
+                }
+                else if(func == 3)
+                {
+                    for(int i=0; i<9; i++)
+                    {
+                        for(int j=0; j<8; j++)
+                        {
+                            Console.Write(String.Format("{0,-6}", curri[i, j]));
+                        }
+                        Console.WriteLine();
+                    }
+                }else if(func == 4)
+                {
+                    Console.WriteLine(credits);
+                }else if(func == 5)
+                {
+                    ;
+                }
+            } while (func != 5);
+        }
+    }
+}
---
+++ f74104040_practice_2/F74104040_practice_2_2/practice_2_2/Program.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] board = new char[9, 9];
+            char[] op = new char[2];
+            bool player = true;
+            int O_num = 0;
+            int X_num = 0;
+
+            //initialize the board
+            board[0, 0] = ' ';
+            char a = '1';
+            for (int i=1; i<board.GetLength(0); i++)
+            {
+                board[i, 0] = a;
+                a++;
+            }
+            char tmp = 'A';
+            for (int j=1; j<board.GetLength(1); j++)
+            {
+                board[0, j] = tmp;
+                tmp++;
+            }
+            for(int i=1; i<board.GetLength(0); i++)
+            {
+                for(int j=1; j<board.GetLength(1); j++)
+                {
+                    board[i, j] = '-';
+                }
+            }
+
+            print(ref board);
+            do
+            {
+                if (player)
+                {
+                    Console.WriteLine("It is player O's turn, please enter the position: ");
+                    op = Console.ReadLine().ToCharArray();
+                    if(board[op[1] - '1' + 1, op[0] - 'A' + 1] == '-')
+                    {
+                        board[op[1] - '1' + 1, op[0] - 'A' + 1] = 'O';
+                        O_num++;
+
+                        check(op[1] - '1' + 1, op[0] - 'A' + 1, ref X_num, ref O_num, ref board, player);
+                        Console.Clear();
+                        print(ref board);
+                        player = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("There is a chess piece in this position, please press a key to continue");
+                        Console.ReadKey(); // let user to press any key
+                        Console.Clear();
+                        print(ref board);
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("It is player X's turn, please enter the position: ");
+                    op = Console.ReadLine().ToCharArray();
+                    if (board[op[1] - '1' + 1, op[0] - 'A' + 1] == '-')
+                    {
+                        board[op[1] - '1' + 1, op[0] - 'A' + 1] = 'X';
+                        X_num++;
+
+                        check(op[1] - '1' + 1, op[0] - 'A' + 1, ref X_num, ref O_num, ref board, player);
+                        Console.Clear();
+                        print(ref board);
+                        player = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("There is a chess piece in this position, please press a key to continue");
+                        Console.ReadKey(); // let user to press any key
+                        Console.Clear();
+                        print(ref board);
+                    } 
+                    
+                }
+                
+            } while ((O_num + X_num) != 64);
+
+            if(O_num > X_num)
+            {
+                Console.WriteLine("Game Over, Winner is player O !");
+            }
+            else
+            {
+                Console.WriteLine("Game Over, Winner is player X !");
+            }
+            Console.ReadLine();
+        }
+
+        static void print(ref char[,] board)
+        {
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    Console.Write(board[i, j]);
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void check(int row, int col, ref int X_num, ref int O_num, ref char[,] board, bool player)
+        {
+            int next = row;
+            if (player)//O
+            {
+                // check the column down
+                for(int i=row; i<8; i++)
+                {
+                    if (board[i + 1, col] == '-') continue;
+                    else if(board[i + 1, col] == 'O')
+                    {
+                        for(int j=i; j>next; j--)
+                        {
+                            if(board[j,col] == 'X')
+                            {
+                                board[j, col] = 'O';
+                                X_num--;
+                                O_num++;
+                            }
+                        }
+                        next = i + 1;
+                        Console.WriteLine(next);  // This is a annotation
+                    }
+                }
+
+                next = row;
+
+                // check the column up
+                for (int i=row; i>0; i--)
+                {
+                    if (board[i - 1, col] == '-') continue;
+                    else if(board[i - 1,col] == 'O')
+                    {
+                        for (int j=i; j<next; j++)
+                        {
+                            if (board[j, col] == 'X')
+                            {
+                                board[j, col] = 'O';
+                                X_num--;
+                                O_num++;
+                            }
+                        }
+                        next = i - 1;
+                    }
+                }
+
+                next = col;
+
+                //check the row right
+                for(int i=col; i<8; i++)
+                {
+                    if (board[row, i + 1] == '-') continue;
+                    else if(board[row, i + 1] == 'O')
+                    {
+                        for(int j=i; j>next; j--)
+                        {
+                            if(board[row, j] == 'X')
+                            {
+                                board[row, j] = 'O';
+                                X_num--;
+                                O_num++;
+                            }
+                        }
+                        next = i + 1;
+                    }
+                }
+
+                next = col;
+
+                //check the row left
+                for (int i = col; i > 0; i--)
+                {
+                    if (board[row, i - 1] == '-') continue;
+                    else if (board[row, i - 1] == 'O')
+                    {
+                        for (int j = i; j < next; j++)
+                        {
+                            if (board[row, j] == 'X')
+                            {
+                                board[row, j] = 'O';
+                                X_num--;
+                                O_num++;
+                            }
+                        }
+                        next = i - 1;
+                    }
+                }
+            }
+            else //X
+            {
+                // check the column down
+                for (int i = row; i < 8; i++)
+                {
+                    if (board[i + 1, col] == '-') continue;
+                    else if (board[i + 1, col] == 'X')
+                    {
+                        for (int j = i; j > next; j--)
+                        {
+                            if (board[j, col] == 'O')
+                            {
+                                board[j, col] = 'X';
+                                X_num++;
+                                O_num--;
+                            }
+                        }
+                        next = i + 1;
+                        Console.WriteLine(next);  // This is a annotation
+                    }
+                }
+
+                next = row;
+
+                // check the column up
+                for (int i = row; i > 0; i--)
+                {
+                    if (board[i - 1, col] == '-') continue;
+                    else if (board[i - 1, col] == 'X')
+                    {
+                        for (int j = i; j < next; j++)
+                        {
+                            if (board[j, col] == 'O')
+                            {
+                                board[j, col] = 'X';
+                                X_num++;
+                                O_num--;
+                            }
+                        }
+                        next = i - 1;
+                    }
+                }
+
+                next = col;
+
+                //check the row right
+                for (int i = col; i < 8; i++)
+                {
+                    if (board[row, i + 1] == '-') continue;
+                    else if (board[row, i + 1] == 'X')
+                    {
+                        for (int j = i; j > next; j--)
+                        {
+                            if (board[row, j] == 'O')
+                            {
+                                board[row, j] = 'X';
+                                X_num++;
+                                O_num--;
+                            }
+                        }
+                        next = i + 1;
+                    }
+                }
+
+                next = col;
+
+                //check the row left
+                for (int i = col; i > 0; i--)
+                {
+                    if (board[row, i - 1] == '-') continue;
+                    else if (board[row, i - 1] == 'X')
+                    {
+                        for (int j = i; j < next; j++)
+                        {
+                            if (board[row, j] == 'O')
+                            {
+                                board[row, j] = 'X';
+                                X_num++;
+                                O_num--;
+                            }
+                        }
+                        next = i - 1;
+                    }
+                }
+            }
+            
+        }
+    }
+}
